576e0ce2896628fb072a4c81b4a7056cb5c3e66e
Amran Yobioktabera <amran.yobi@gmail.com>
2023-07-16 01:48:47 +0700

    perubahan banyak

--- application/views/admin/dasbor/lelang_berjalan.php
+++ application/views/admin/dasbor/lelang_berjalan.php
@@ -108,11 +108,181 @@
   </script>
 </head>
 <?php
-if($status_voucher['status']=='1')
+if(isset($status_voucher))
 {
-    if(isset($data_lelang)){
-      if($data_lelang['judul_lelang']=='')
-      {
+  if($status_voucher['status']=='1')
+  {
+      if(isset($data_lelang)){
+        if($data_lelang['judul_lelang']=='')
+        {
+          ?>
+          <div class="card-body">
+            <div class="callout callout-danger">
+              <h5>Tidak ada lelang!</h5>
+              <p>Mohon maaf, untuk saat ini tidak ada lelang yang sedang berjalan</p>
+            </div>
+          </div>
+          <?php
+        }else{
+          ?>
+          <section class="content">
+            <div class="card card-solid">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-6">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                              <h1 id="counter" class="text-center m-auto p-3 text-white"></h1>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="alert alert-success alert-dismissible">
+                      <h5><i class="fa fa-calendar"></i> Periode Lelang</h5>
+                      <h4><?php echo date("d-m-Y");?></h4>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-sm-6">
+                    <div class="container">
+                        <div class="slider">
+                            <div>
+                                <a href="#">
+                                    <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar']) ?>" alt="Image 1">
+                                </a>            
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar2']) ?>" alt="Image 2">
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar3']) ?>" alt="Image 3">
+                                </a>            
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar4']) ?>" alt="Image 4">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-12 col-sm-6">
+                    <div class="card">
+                      <div class="card-header">
+                        <h3 class="card-title">Spesifikasi</h3>
+                        <div class="card-tools">
+                        </div>
+                      </div>
+                      <div class="card-body p-0">
+                        <ul class="nav nav-pills flex-column">
+                          <li class="nav-item active">
+                            <a href="#" class="nav-link">
+                              <i class="fa fa-gears"></i> Model
+                              <font size="5"><span class="badge bg-primary float-right"><?php echo $data_lelang['judul_lelang']?></span></font>
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#" class="nav-link">
+                              <i class="fa fa-bookmark"></i> Tipe
+                              <font size="5"><span class="badge bg-warning float-right"><?php echo $data_lelang['transmisi']?></span></font>
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#" class="nav-link">
+                              <i class="fa fa-magic"></i> Warna
+                              <font size="5"><span class="badge bg-warning float-right"><?php echo $data_lelang['warna']?></span></font>
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a href="#" class="nav-link">
+                              <i class="fa fa-calendar"></i> Tahun
+                              <font size="5"><span class="float-right"><?php echo $data_lelang['tahun']?></span></font>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <h4 class="mt-3">Grade</h4>
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                      <label class="btn btn-default text-center">
+                        <input type="radio" name="color_option" id="color_option_b1" autocomplete="off">
+                        <span class="text-xl"><?php echo $data_lelang['grade']?></span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <nav class="w-100">
+                      <div class="nav nav-tabs" id="product-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="product-desc-tab" data-toggle="tab" href="#product-desc" role="tab" aria-controls="product-desc" aria-selected="true">Deskripsi</a>
+                      </div>
+                    </nav>
+                    <div class="tab-content p-3" id="nav-tabContent">
+                      <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"> <?php echo $data_lelang['isi']?></div>
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <h4 class="mt-3">Harga Dasar</h4>
+                    <div class="bg-gray py-2 px-3 mt-4">
+                      <h2 class="mb-0">
+                        Rp. <?php echo number_format ($data_lelang['harga_awal'])?>
+                      </h2>
+                      <!-- <h4 class="mt-0">
+                        <small>Ex Tax: $80.00 </small>
+                      </h4> -->
+                    </div>
+                  </div>
+                </div>
+                <div class="row" style="margin-top: 10px;">
+                  <!-- <div class="col-6">
+                    <h4 class="mt-3">Bid Berjalan</h4>
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>No</th>
+                          <th>Harga Bid</th>
+                          <th>Waktu Bid</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Call of Duty</td>
+                          <td>455-981-221</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Need for Speed IV</td>
+                          <td>247-925-726</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div> -->
+                  <div class="col-md-6" id="tampil">
+
+                  </div>
+                  <div class="col-6">
+                    <form id="form">
+                      <button id="tombol_tambah" type="button" class="btn btn-lg btn-primary" data-dismiss="modal" >BID</button>
+                    </form>
+                    <!-- <a href="" class="btn btn-primary btn-lg btn-flat">BID</a> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <?php
+        }
+      }else{
         ?>
         <div class="card-body">
           <div class="callout callout-danger">
@@ -121,175 +291,18 @@ if($status_voucher['status']=='1')
           </div>
         </div>
         <?php
-      }else{
-        ?>
-        <section class="content">
-          <div class="card card-solid">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-6">
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-md-12">
-                          <div class="card-body">
-                            <h1 id="counter" class="text-center m-auto p-3 text-white"></h1>
-                          </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-6">
-                  <div class="alert alert-success alert-dismissible">
-                    <h5><i class="fa fa-calendar"></i> Periode Lelang</h5>
-                    <h4><?php echo date("d-m-Y");?></h4>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12 col-sm-6">
-                  <div class="container">
-                      <div class="slider">
-                          <div>
-                              <a href="#">
-                                  <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar']) ?>" alt="Image 1">
-                              </a>            
-                          </div>
-                          <div>
-                              <a href="#">
-                                  <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar2']) ?>" alt="Image 2">
-                              </a>
-                          </div>
-                          <div>
-                              <a href="#">
-                                  <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar3']) ?>" alt="Image 3">
-                              </a>            
-                          </div>
-                          <div>
-                              <a href="#">
-                                  <img src="<?php echo base_url('assets/upload/image/thumbs/'.$data_lelang['gambar4']) ?>" alt="Image 4">
-                              </a>
-                          </div>
-                      </div>
-                  </div>
-                </div>
-                <div class="col-12 col-sm-6">
-                  <div class="card">
-                    <div class="card-header">
-                      <h3 class="card-title">Spesifikasi</h3>
-                      <div class="card-tools">
-                      </div>
-                    </div>
-                    <div class="card-body p-0">
-                      <ul class="nav nav-pills flex-column">
-                        <li class="nav-item active">
-                          <a href="#" class="nav-link">
-                            <i class="fa fa-gears"></i> Model
-                            <font size="5"><span class="badge bg-primary float-right"><?php echo $data_lelang['judul_lelang']?></span></font>
-                          </a>
-                        </li>
-                        <li class="nav-item">
-                          <a href="#" class="nav-link">
-                            <i class="fa fa-bookmark"></i> Tipe
-                            <font size="5"><span class="badge bg-warning float-right"><?php echo $data_lelang['transmisi']?></span></font>
-                          </a>
-                        </li>
-                        <li class="nav-item">
-                          <a href="#" class="nav-link">
-                            <i class="fa fa-magic"></i> Warna
-                            <font size="5"><span class="badge bg-warning float-right"><?php echo $data_lelang['warna']?></span></font>
-                          </a>
-                        </li>
-                        <li class="nav-item">
-                          <a href="#" class="nav-link">
-                            <i class="fa fa-calendar"></i> Tahun
-                            <font size="5"><span class="float-right"><?php echo $data_lelang['tahun']?></span></font>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <h4 class="mt-3">Grade</h4>
-                  <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label class="btn btn-default text-center">
-                      <input type="radio" name="color_option" id="color_option_b1" autocomplete="off">
-                      <span class="text-xl"><?php echo $data_lelang['grade']?></span>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-6">
-                  <nav class="w-100">
-                    <div class="nav nav-tabs" id="product-tab" role="tablist">
-                      <a class="nav-item nav-link active" id="product-desc-tab" data-toggle="tab" href="#product-desc" role="tab" aria-controls="product-desc" aria-selected="true">Deskripsi</a>
-                    </div>
-                  </nav>
-                  <div class="tab-content p-3" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"> <?php echo $data_lelang['isi']?></div>
-                  </div>
-                </div>
-                <div class="col-6">
-                  <h4 class="mt-3">Harga Dasar</h4>
-                  <div class="bg-gray py-2 px-3 mt-4">
-                    <h2 class="mb-0">
-                      Rp. <?php echo number_format ($data_lelang['harga_awal'])?>
-                    </h2>
-                    <!-- <h4 class="mt-0">
-                      <small>Ex Tax: $80.00 </small>
-                    </h4> -->
-                  </div>
-                </div>
-              </div>
-              <div class="row" style="margin-top: 10px;">
-                <!-- <div class="col-6">
-                  <h4 class="mt-3">Bid Berjalan</h4>
-                  <table class="table table-striped">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>Harga Bid</th>
-                        <th>Waktu Bid</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Call of Duty</td>
-                        <td>455-981-221</td>
-                      </tr>
-                      <tr>
-                        <td>1</td>
-                        <td>Need for Speed IV</td>
-                        <td>247-925-726</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div> -->
-                <div class="col-md-6" id="tampil">
-
-                </div>
-                <div class="col-6">
-                  <form id="form">
-                    <button id="tombol_tambah" type="button" class="btn btn-lg btn-primary" data-dismiss="modal" >BID</button>
-                  </form>
-                  <!-- <a href="" class="btn btn-primary btn-lg btn-flat">BID</a> -->
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <?php
       }
-    }else{
-      ?>
-      <div class="card-body">
-        <div class="callout callout-danger">
-          <h5>Tidak ada lelang!</h5>
-          <p>Mohon maaf, untuk saat ini tidak ada lelang yang sedang berjalan</p>
-        </div>
+  }else{
+    ?>
+    <div class="card-body">
+      <div class="callout callout-danger">
+        <h5>Anda tidak memiliki Voucher Lelang Aktif!</h5>
+        <p>Mohon maaf, Anda tidak memiliki voucher lelang yang aktif. Beli Voucher Lelang terlebih dahulu atau selesaikan proses pembelian voucher jika Anda masih dalam proses pembelian voucher</p>
       </div>
-      <?php
-    }
+    </div>
+    <?php
+
+  }
 }else{
   ?>
   <div class="card-body">
